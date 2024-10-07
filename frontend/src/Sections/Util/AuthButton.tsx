1b1cb2b803009d40f680759355f157bbfb7a037a
JWH <jwheneghan@hotmail.com>
2024-10-07 15:50:32 -0230

    connected AuthButton to correct path on server

--- frontend/src/Sections/Util/AuthButton.tsx
+++ frontend/src/Sections/Util/AuthButton.tsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import CircularProgress from '@mui/material/CircularProgress';
 
 interface AuthButtonProps {
     setAuthStatus: (authStatus:boolean) => void;
@@ -10,31 +12,47 @@ interface AuthButtonProps {
 const AuthButton = ({setAuthStatus}:AuthButtonProps) => {
 
     const [callStatus, setCallStatus] = React.useState<boolean>(false)
+    const [isAuthorizing, setIsAuthorizing] = React.useState<boolean>(false)
 
     React.useEffect(()=> {
         if(callStatus) {
+            setIsAuthorizing(true)
             const callBackEnd = async () => {
-                const endPoint = 'http://localhost:5000/test'
+                const endPoint = 'http://localhost:5000/auth/yahoo'
 
                 try {
                     const response = await axios.get(endPoint)
+                    const authUrl = response.data.authUrl
+                    console.log(authUrl)
+                    window.open(authUrl, '_blank')
                     if(response.data.status == 'success') {
                         setAuthStatus(true)
+                        setIsAuthorizing(false)
                     }
                 } catch (error) {
                     setAuthStatus(false)
+                    setIsAuthorizing(false)
                     console.log(error)
                 }
             }
             callBackEnd()
         }
-    },[callStatus, setAuthStatus])
+    },[callStatus, setAuthStatus, setIsAuthorizing])
     return (
-        <Box sx={{ '& button': { m: 1 } }}>
-            <Button size='large' onClick={()=> {
-                setCallStatus(true)
-            }}>Authorize With Yahoo</Button>
-        </Box>
+        <>
+        {!isAuthorizing && 
+            <Box sx={{ '& button': { m: 1 } }}>
+                <Button size='large' onClick={()=> {
+                        setCallStatus(true)
+                        }}>Authorize With Yahoo</Button>
+            </Box>
+        }
+        {isAuthorizing &&
+                    <Stack sx={{ color: 'grey.500' }} spacing={2} direction="row">
+                    <CircularProgress color="secondary" />
+                </Stack>
+                }
+        </>
     )
 }
 
